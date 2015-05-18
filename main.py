eed6c917bf116fca9e0f529d293e07fc7eab18e3
redstorm45 <pierre.cadart@gmail.com>
2015-05-18 18:13:55 +0200

    Premiers fichiers
    ajout du rapport et du fichier principal du programme

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+
+Ce projet est un essai de routage similaire à celui du web,
+mais appliqué au bluetooth.
+
+Le routeur est un programme hôte des périphériques,
+permettant un mappage réseau (découverte réseau),
+et du transfert de donnée d'un réseau à un autre.
+
+Tous les périphériques possèdent un port utilisé par le serveur,
+et un autre pour le client. Ainsi, il se connecte à tous les périphériques
+qu'il trouve à proximité avec ce protocole
+
+Ce programme utilisera la bibliothèque pybluez
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
