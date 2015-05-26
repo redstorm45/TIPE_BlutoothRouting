4ad825cf87332b4fdff3afd7d61916eeea83fd24
redstorm45 <pierre.cadart@gmail.com>
2015-05-26 23:16:16 +0200

    du contenu
    +ajout de la fonction de recherche
    +clarifié la structure

--- main.py
+++ main.py
@@ -12,18 +12,45 @@
     Tous les périphériques possèdent un port utilisé par le serveur,
     et un autre pour le tunnel. Ainsi, il se connecte à tous les
     périphériques qu'il trouve à proximité avec ce protocole, et peut
-    transmettre des données par la connexion tunnel.
+    transmettre des données par la connection tunnel.
 
     Ce programme utilise la bibliothèque pybluez
 
     versions:
         python 3.3.5
         pybluez 0.20 for python 3.3
-
+        
+        
+    :var mappageReseau:
+          dictionnaire de toutes les addresses connues de ce point
+          où chaque entrée est repéree par son adresse MAC
+          
+          attributs:
+            nom : nom lisible du périphérique
+            direct : le périphérique est-il visible d'ici
+            avance : le périphérique a-t-il un serveur
+            liens : périphériques contactables à travers celui-ci
+          
+        exemple de dictionnaire:
+          {
+            "XX:XX:XX:XX:00" : {"nom": "ordi1" ,
+                                "direct": True ,
+                                "avance": True,
+                                "liens": <liste de proximité de ordi1>} ,
+            "XX:XX:XX:XX:01" : {"nom": "ordi2" ,
+                                "direct": False ,
+                                "avance": True,
+                                "liens": <liste de proximité de ordi2>} ,
+            "XX:XX:XX:XX:02" : {"nom": "telephone" ,
+                                "direct": False ,
+                                "avance": False,
+                                "liens": <liste de proximité de telephone>}
+          }
 """
 
 import bluetooth
 import threading
+import time
 
 ##  paramètres du programme
 
@@ -36,8 +63,11 @@ UUID_Tunnel  = "67b7a1d1-fd7b-11e4-b939-0800200c9a66"
 #socket du serveur
 socketServeur = None
 #recherches
-rechercheEnCours = {}
-
+peripheriquesContactables = []
+peripheriquesAdjacents = []
+rechercheLancees = 0
+#mappage réseau à partir de ce point
+mappageReseau = {}
 
 ## classe
 
@@ -56,7 +86,7 @@ class SocketServeur(bluetooth.BluetoothSocket):
         # applique le socket sur le premier adaptateur trouvé,
         # et le premier port libre
         self.bind( ("",bluetooth.PORT_ANY) )
-        # commence l'écoute sur le port, avec 1 connexion
+        # commence l'écoute sur le port, avec une connection
         # en file d'attente au maximum
         self.listen(1)
         # averti le serveur SDP de la présence du serveur
@@ -98,21 +128,23 @@ class SocketServeur(bluetooth.BluetoothSocket):
         print("recu paquet:\n",dat)
         liste = dat.split(",")
         if liste[0] == "decouverte":
-            pass
+            #demande de découverte réseau en profondeur
+            addresses = [ ( i.split("/")[0] , int(i.split("/")[1]) ) for i in liste[1:] ]
+            decouverteReseau(adresses)
     
     def envoiePaquet(self,dest,dat):
         """
             Permet d'envoyer un paquet de donnée à un destinataire
             
-            :param dest: tuple de le forme (host,channel)
+            :param dest: tuple de forme (host,channel)
         """
         #trouve la taille du paquet
-        tailleMessage = len(dat.toBytes())
+        tailleMessage = len( bytes(dat,encoding="utf-8") )
         #crée le paquet
         message = str(tailleMessage) + ";" + dat
         #ouvre une connection
         sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-        sock.connect( dest )
+        sock.connect( (dest[0] , bluetooth.PORT_ANY) )
         #envoie le message
         sock.send(message)
         #ferme la connection
@@ -149,7 +181,7 @@ def bouclePrincipale():
     global socketServeur
     #boucle
     while True:
-        #attend des connexions
+        #attend des connections
         extSocket , address = socketServeur.accept()
         #ajoute la connection à la liste actuelle
         socketServer.connections.append( [ address , extSocket ] )
@@ -158,9 +190,90 @@ def bouclePrincipale():
         dat.daemon = True
         dat.start()
 
-def decouverteReseau(periph,time=5):
+def rechercheStandard():
     """
-        Permet d'accéder à tous les périphérques à proximité qui
+        Effectue une recherche rapide (sans contacter
+        les autres périphériques)
+    """
+    #effectue une recherche
+    pairs = bluetooth.discover_devices()
+    #associe les noms
+    periph = []
+    for i in pairs:
+        periph.append( (i,bluetooth.lookup_name(i)) )
+    #met à jour la liste
+    global peripheriquesAdjacents
+    peripheriquesAdjacents  = pairs[:]
+    #met à jour le mappage
+    mappageDepuisListes()
+    #retourne la liste nommée
+    return periph
+
+def decouverteReseau(periph):
+    """
+        Permet d'accéder à tous les périphériques à proximité qui
+        possèdent le service de réseau actif, et leurs demander
+        d'effectuer à leur tour une découverte réseau.
+        
+        La découverte s'effectue en deux temps:
+          1 - élements contactables:
+              trouve les périphériques pouvant mapper le réseau
+              (avec le service démarré)
+          2 - élements atteignables:
+              affecte à chaque periphérique actif une liste
+              d'autres périphériques qu'il peut atteindre
+        
+        Cette recherche peut prendre du temps
+        
+        :param periph: liste des périphériques déjà découverts
+                       sous forme d'un tuple ( adresse MAC , port RFCOMM )
+        
+        :return: None
+    """
+    #recherche les périphériques à proximité qui ont ce programme
+    print("trouve le service")
+    liste = bluetooth.find_service("Paquet",UUID_Serveur)
+    print("trouvé : ",liste)
+    liste = [ i["host"] for i in liste ]
+    #affecte à la liste locale
+    global peripheriquesContactables
+    peripheriquesContactables = liste[:]
+    #ajoute l'adresse actuelle à la liste
+    periph.append( socketServeur.getsockname() )
+    #supprime les éléments déjà inspectés
+    for item in liste:
+        if item[0] in periph:
+            liste.remove(item)
+    #crée l'argument du paquet de recherche
+    arg = ",".join( [ item[0] + "/" + str(item[1]) for item in liste ] )
+    #demande aux periphériques d'effectuer leur recherche
+    for add in liste:
+        socketServeur.envoiePaquet( add , "decouverte," + arg )
+    #effectue une recherche locale
+    local = listeStandard(time)
+    #affecte à la liste locale
+    global peripheriquesAdjacents
+    peripheriquesAdjacents = liste
+
+def mappageDepuisListes():
+    """
+        Met à jour le mappage, à partir des liste de connections disponibles
+    """
+    for p in peripheriquesAdjacent:
+        #crée un nouvel élément
+        item = { "nom": bluetooth.lookup_name(p) ,
+                 "direct": True ,
+                 "avance": p in peripheriquesContactables,
+                 "liens": [] }
+        #modifie si déjà ds le mappage actuel
+        if p in mappageReseau.keys():
+            item["liens"] = mappageReseau[p]["liens"]
+        #ajout au mappage
+        mappageReseau[p] = item
+
+def rechercheReseau(periph):
+    """
+        Permet d'accéder à tous les périphériques à proximité qui
         possèdent le service de réseau actif, et leurs demander
         d'effectuer à leur tour une recherche réseau.
         
@@ -171,8 +284,7 @@ def decouverteReseau(periph,time=5):
               affecte à chaque periphérique actif une liste
               d'autres périphériques qu'il peut atteindre
         
-        Il est possible que cette recherche prenne du temps
-        la démarrer dans un autre thread?
+        Cette recherche peut prendre du temps
         
         :param periph: liste des périphériques déjà découverts
                        sous forme d'adresse MAC
@@ -181,10 +293,15 @@ def decouverteReseau(periph,time=5):
         :return: None
     """
     #recherche les périphériques à proximité qui ont ce programme
-    liste = bluetooth.find_service("Paquet",UUID_serveur)
+    print("trouve le service")
+    liste = bluetooth.find_service("Paquet",UUID_Serveur)
+    print("trouvé : ",liste)
     liste = [ i["host"] for i in liste ]
+    #affecte à la liste locale
+    global peripheriquesContactables
+    peripheriquesContactables = liste
     #ajoute l'adresse actuelle à la liste
-    liste.append( socketServeur.getpeername() )
+    periph.append( socketServeur.getsockname() )
     #supprime les éléments déjà inspectés
     for item in liste:
         if item[0] in periph:
@@ -192,75 +309,17 @@ def decouverteReseau(periph,time=5):
     #crée l'argument du paquet de recherche
     arg = ",".join( [ item[0] + "/" + str(item[1]) for item in liste ] )
     #demande aux periphériques d'effectuer leur recherche
-    for add in list:
+    global rechercheLancees
+    rechercheLancee = 0
+    for add in liste:
         socketServeur.envoiePaquet( add , "decouverte," + arg )
-
-def listeComplet():
-    """
-        Donne une liste complète des périphériques du réseau
-        
-        :return: liste des éléments par leur adresses
-            
-        :example:
-            
-            réseau comme suit: (D0 point de départ)
-            
-                    D0 --- D1
-                      \--- D2 --- D3
-            
-            return:
-                [ 
-                  { 
-                    "nom" : "D1",
-                    "adress" : "XX:XX:XX:XX:X1"
-                  } ,
-                  { 
-                    "nom" : "D2",
-                    "adress" : "XX:XX:XX:XX:X2"
-                  } ,
-                  { 
-                    "nom" : "D3",
-                    "adress" : "XX:XX:XX:XX:X3"
-                  }
-                ]
-        
-    """
-    
-def mappageComplet():
-    """
-        Donne une carte complète du réseau,
-        sous forme de dictionnaires imbriqués
-        
-        :return: liste des éléments par leur adresses
-        
-        :example:
-            
-            réseau comme suit: (D0 point de départ)
-            
-                    D0 --- D1
-                      \--- D2 --- D3
-            
-            return:
-                [ 
-                  { 
-                    "nom" : "D1",
-                    "adress" : "XX:XX:XX:XX:X1"
-                  } ,
-                  { 
-                    "nom" : "D2",
-                    "adress" : "XX:XX:XX:XX:X2",
-                    "pairs" : 
-                    [
-                      { 
-                        "nom" : "D3",
-                        "adress" : "XX:XX:XX:XX:X3"
-                      }
-                    ]
-                  }
-                ]
-        
-    """
-
+        rechercheLancees += 1
+    #effectue une recherche locale
+    local = listeStandard() #mappage màj en mm temps
+    #attends les réponses
+    print("attends retour...")
+    while rechercheLancees > 0:
+        time.sleep(0.1)
 
 ## début du programme
 
@@ -276,12 +335,20 @@ main.start()
 continuer = True
 while continuer:
     print("   **** MENU ****   ")
-    print(" 1) découverte standard")
-    print(" 2) découverte avancée")
-    print(" 3) mappage réseau")
-    print(" 4) quitter")
+    print(" 1) découverte:")
+    print("     demande à chaque périphérique du réseau")
+    print("     d'effectuer une recherche")
+    print(" 2) recherche standard:")
+    print("     trouve tous les périphériques contactables")
+    print("     à proximité")
+    print(" 3) recherche avancée:")
+    print("     trouve tous les périphériques contactables")
+    print("     à travers le réseau")
+    print(" 4) mappage réseau:")
+    print("     affiche une carte du réseau")
+    print(" 5) quitter")
     
-    k = input("press enter to end")
+    k = input(":")
     try:
         k = int(k)
     except:
@@ -291,15 +358,19 @@ while continuer:
             print("\nentrez un vrai choix\n")
         else:
             if k == 1:
-                #découverte bluetooth standard
-                pass
+                #demande à chaque périphérique de découvrir son réseau
+                decouverteReseau([])
             elif k == 2:
-                #découverte multi-étage
-                pass
+                #découverte bluetooth standard
+                liste = rechercheStandard()
+                print(liste)
             elif k == 3:
+                #découverte bluetooth avancée
+                rechercheReseau()
+            elif k == 4:
                 #cherche les amis, pour donner une carte
                 pass
-            elif k == 4:
+            elif k == 5:
                 #quitter :-(
                 continuer = False
 
